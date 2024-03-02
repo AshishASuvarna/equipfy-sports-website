b23cfb28682d4e3ab7625fe615a3bf146ed001f4
AshishASuvarna <155023418+AshishASuvarna@users.noreply.github.com>
2024-03-02 08:11:55 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+ITEMS =[
+  {
+    'id': 1,
+    'title': 'Shirt',
+    'price': '10',
+    'rating': 4.5
+  },
+  {
+    'id': 2,
+    'title': 'Watch',
+    'price': '50',
+    'rating': 4
+  },
+  {
+    'id': 3,
+    'title': 'Shoe',
+    'price': '35',
+    'rating': 5
+  },
+  {
+    'id': 4,
+    'title': 'Shoe',
+    'price': '35',
+    'rating': 5
+  },
+  {
+    'id': 5,
+    'title': 'Shoe',
+    'price': '35',
+    'rating': 5
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return "Hello World!"
+  return render_template('home.html')
 
+# @app.route("/clothing")
+# def clothing():
+#   return render_template('clothing.html', items=ITEMS)
+
+@app.route("/api/clothing")
+def clothing():
+  return jsonify(ITEMS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
